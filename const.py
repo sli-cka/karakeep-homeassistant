18c1f23016aff4f975ebc78d0f0255af164711fe
sli-ckam <flo.mang@gmail.com>
2025-11-17 10:37:07 +0100

    change folder structure for hacs

--- const.py
+++
@@ -1,5 +0,0 @@
-DOMAIN = "karakeep"
-
-DEFAULT_SCAN_INTERVAL = 300  # seconds
-CONF_SCAN_INTERVAL = "scan_interval"
-PLATFORMS = ["sensor"]
---
+++ custom_components/karakeep/const.py
@@ -0,0 +1,5 @@
+DOMAIN = "karakeep"
+
+DEFAULT_SCAN_INTERVAL = 300  # seconds
+CONF_SCAN_INTERVAL = "scan_interval"
+PLATFORMS = ["sensor"]
